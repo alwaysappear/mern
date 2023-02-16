347c41267e5a2af613764b4410b263a487222c99
Raheem Kawojue <kawojue08@gmail.com>
2023-02-16 19:54:13 +0100

    ...

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const connectDB = require('./config/dbConn')
+const corsOptions = require('./config/corsOptions')
+const errHandler = require('./middlewares/errHandler')
 
 const app = express()
 const PORT = process.env.PORT
@@ -13,13 +15,16 @@ connectDB()
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-    origin: ['http://localhost:3500'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(cors(corsOptions))
+
+app.use(errHandler)
 
 app.get('^/$|i/ndex(.html)', (req, res) => {
     res.status(200).json({ message: 'Successful! '})
+})
+
+app.get('*', (req, res) => {
+    res.status(404).json({ message: 'Page not found!'})
 })
 
 mongoose.connection.once('open', () => {
