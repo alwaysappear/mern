3b121d6c75d2d587fc339e0d2ee3702460511a80
Raheem Kawojue <kawojue08@gmail.com>
2023-02-18 02:53:47 +0100

    [fix] db conn

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
+const connectDB = () => {
     try {
-        await mongoose.connect(
+        mongoose.connect(
             process.env.DATABASE_URI,
             {
                 useUnifiedTopology: true,
