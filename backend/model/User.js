c695bb8e7533114c8640050ae5e63f252ae35c33
Raheem Kawojue <kawojue08@gmail.com>
2023-02-19 20:49:33 +0100

    [add] user schema

---
+++ backend/model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Admin: Number,
+        Employee: Number,
+        Manager: Number
+    },
+    creationDate: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+export default mongoose.Model('userModel', userSchema)
