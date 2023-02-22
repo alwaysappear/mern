91a033c29dc2a707b839fc9732ff787b41b002b7
Raheem Kawojue <kawojue08@gmail.com>
2023-02-22 13:27:34 +0100

    get all users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcrypt')
 const Note = require('../model/Note')
-const Users = require('../model/User')
+const User = require('../model/User')
 const asyncHandler = require('express-async-handler')
 
 
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select('-pswd').lean()
+    if (!users) return res.status(400).json({ message: "Users not found." })
+    return res.status(200).json(users)
 })
 
 const createNewUser = asyncHandler(async (req, res) => {
-
+    
 })
 
 const updateUser = asyncHandler(async (req, res) => {
