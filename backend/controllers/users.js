7efbb9d5bce069cf576d79443c7278c38ca7826c
Raheem Kawojue <kawojue08@gmail.com>
2023-02-22 13:02:27 +0100

    [set] skeleton

---
+++ backend/controllers/users.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt')
+const Note = require('../model/Note')
+const Users = require('../model/User')
+const asyncHandler = require('express-async-handler')
+
+
+const getAllUsers = asyncHandler(async (req, res) => {
+
+})
+
+const createNewUser = asyncHandler(async (req, res) => {
+
+})
+
+const updateUser = asyncHandler(async (req, res) => {
+
+})
+
+const deleteUser = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = {
+    getAllUsers, updateUser,
+    createNewUser, deleteUser
+}
