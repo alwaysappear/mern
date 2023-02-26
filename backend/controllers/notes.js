4ced28e60f065d2e5fdffa46c44ac33bb6edf2f9
Raheem Kawojue <kawojue08@gmail.com>
2023-02-26 15:57:04 +0100

    [edit] setup funcs

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,5 +1,10 @@
 const Note = require('../model/Note')
+const User = require('../model/User')
 const asyncHandler = require('express-async-handler')
+
+const getAllNotes = async(async (req, res) => {
+
+})
 
 const addNote = asyncHandler(async (req, res) => {
 
@@ -10,10 +15,10 @@ const editNote = asyncHandler(async (req, res) => {
 })
 
 const deleteNote = asyncHandler(async (req, res) => {
-    
+
 })
 
 module.exports = {
     addNote, editNote,
-    deleteNote
+    deleteNote, getAllNotes
 }
