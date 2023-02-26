f091fc8505cbe7c2f50d3b9fa62fe1ed13552b5b
Raheem Kawojue <kawojue08@gmail.com>
2023-02-26 23:02:13 +0100

    [add] get all notes controller

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -3,7 +3,17 @@ const User = require('../model/User')
 const asyncHandler = require('express-async-handler')
 
 const getAllNotes = async(async (req, res) => {
+    const notes = await Note.find().lean()
+    if (!notes?.length) {
+        return res.status.json({ message: "Empty notes."})
+    }
 
+    const notesWithUser = await Promise.all(notes.map(async note => {
+        const user = await User.findById(note.user).lean().exec()
+        return { ...note, user }
+    }))
+
+    res.json(notesWithUser)
 })
 
 const addNote = asyncHandler(async (req, res) => {
