a92c33601951dad6af4d742852283d07dca75098
Raheem Kawojue <kawojue08@gmail.com>
2023-02-26 15:53:55 +0100

    [add] note controller

---
+++ backend/controllers/notes.js
@@ -0,0 +1,19 @@
+const Note = require('../model/Note')
+const asyncHandler = require('express-async-handler')
+
+const addNote = asyncHandler(async (req, res) => {
+
+})
+
+const editNote = asyncHandler(async (req, res) => {
+
+})
+
+const deleteNote = asyncHandler(async (req, res) => {
+    
+})
+
+module.exports = {
+    addNote, editNote,
+    deleteNote
+}
