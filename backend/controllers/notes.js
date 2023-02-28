2cbae91805ffe58e57a03bce18bf7c96e6ef0da6
Raheem Kawojue <kawojue08@gmail.com>
2023-02-28 13:19:00 +0100

    [add] timestamps

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const Note = require('../model/Note')
 const User = require('../model/User')
 const asyncHandler = require('express-async-handler')
@@ -25,17 +26,59 @@ const addNote = asyncHandler(async (req, res) => {
         })
     }
 
-    await Note.create({
-        user, title, content
+    const newNote = await Note.create({
+        user, title: title.trim(), content: content.trim()
     })
 
-    res.status(201).json({
-        message: "Note saved!"
-    })
+    if (newNote) {
+        res.status(201).json({
+            message: "Note saved!"
+        })
+    } else {
+        res.status(400).json({
+            message: "Uh, oh! Invalid note data recieved."
+        })
+    }
+
+    
 })
 
 const editNote = asyncHandler(async (req, res) => {
+    const { id, user, title, content, completed } = req.body
 
+    if (!id) {
+        return res.status(400).json({
+            message: "Invalid ID."
+        })
+    }
+
+    const note = await Note.findById(user).exec()
+
+    if (!note) {
+        return res.status(404).json({
+            message: "Note not found."
+        })
+    }
+
+    if (note.id !== id) {
+        return res.status(401).json({
+            message: "You can't edit note ID."
+        })
+    }
+
+    if (!content || typeof completed !== 'boolean') {
+        return res.status(400).json({
+            message: "Note not saved. Invalid Input!"
+        })
+    }
+
+    note.title = title.trim()
+    note.content = content.trim()
+    note.completed = completed
+
+    await note.save()
+
+    res.json({ mesaage: "Note Edited!" })
 })
 
 const deleteNote = asyncHandler(async (req, res) => {
